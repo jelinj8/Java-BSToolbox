652255385e084845cf66a8ac383255c269475481
Jakub Jelínek <jakub.jelinek@veropartners.com>
2021-08-31 11:05:50 +0200

    API fix

--- src/cz/bliksoft/javautils/net/Mail.java
+++ src/cz/bliksoft/javautils/net/Mail.java
@@ -10,6 +10,22 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+/**
+ * To uise please add
+ * 
+ * <pre>
+ * <dependency>
+ *  <groupId>com.sun.mail</groupId>
+ *  <artifactId>javax.mail</artifactId>
+ *  <version>1.6.2</version>
+ * </dependency>
+ * </pre>
+ * 
+ * to MVN dependencies.
+ * 
+ * @author jakub
+ *
+ */
 public class Mail {
 	private static Logger log = Logger.getLogger(Mail.class.getName());
 
@@ -19,7 +35,7 @@ public class Mail {
 		Mail.host = host;
 	}
 
-	public static void sendMail(String from, String subject, String messageBody, String... recipients) {
+	public static boolean sendMail(String from, String subject, String messageBody, String... recipients) {
 		//		String host = "localhost";//or IP address  
 
 		//Get the session object  
@@ -40,9 +56,10 @@ public class Mail {
 			// Send message  
 			Transport.send(message);
 			log.info("Message sent.");
-
+			return true;
 		} catch (MessagingException mex) {
 			log.severe("Sending message failed: " + mex.getMessage());
+			return false;
 		}
 	}
 }

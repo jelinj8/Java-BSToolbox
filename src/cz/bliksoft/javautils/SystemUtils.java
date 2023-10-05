353f5bf4b3928643b202981f9b2de32531553b74
Jakub Jelínek <jakub.jelinek@gmail.com>
2023-10-05 12:58:59 +0200

    *

---
+++ src/cz/bliksoft/javautils/SystemUtils.java
@@ -0,0 +1,27 @@
+package cz.bliksoft.javautils;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.logging.Logger;
+
+public class SystemUtils {
+	static Logger log = Logger.getLogger(SystemUtils.class.getName());
+
+	static AtomicBoolean keepWorking = new AtomicBoolean(true);
+
+	public static void keepRunningUntilInterrupted() {
+
+		log.info("Registering shutdown hook.");
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+			keepWorking.set(false);
+		}));
+		
+		log.info("Waiting for shutdown signal.");
+		try {
+			while (keepWorking.get()) {
+				Thread.sleep(1000);
+			}
+		} catch (InterruptedException e) {
+			log.info("Interrupted.");
+		}
+	}
+}

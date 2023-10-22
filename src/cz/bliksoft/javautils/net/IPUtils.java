0bf07e88e48a279f9d17269d5e8474c800b338f7
Jakub Jelínek <jakub.jelinek@gmail.com>
2023-10-22 10:23:26 +0200

    IPUtils

--- src/cz/bliksoft/javautils/net/IPUtils.java
+++ src/cz/bliksoft/javautils/net/IPUtils.java
@@ -1,7 +1,11 @@
 package cz.bliksoft.javautils.net;
 
 import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.net.UnknownHostException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class IPUtils {
 
@@ -12,6 +16,21 @@ public class IPUtils {
 	public static String getHostname(String ip) throws UnknownHostException {
 		InetAddress addr = InetAddress.getByName(ip);
 		return addr.getHostName();
+	}
+
+	private static final String IPV4_PATTERN = "^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\\.(?!$)|$)){4}$";
+
+	private static final Pattern pattern = Pattern.compile(IPV4_PATTERN);
+
+	public static boolean isIPV4(final String email) {
+		Matcher matcher = pattern.matcher(email);
+		return matcher.matches();
+	}
+
+	public static boolean urlIsIPV4(String url) throws MalformedURLException {
+		URL u;
+		u = new URL(url);
+		return isIPV4(u.getHost());
 	}
 
 }

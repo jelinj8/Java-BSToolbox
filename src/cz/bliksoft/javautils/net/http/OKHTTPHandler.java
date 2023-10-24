46bff102b6c28587db5b89a3f231ce10531b1a3f
Jakub Jelínek <jakub.jelinek@gmail.com>
2023-10-24 09:41:23 +0200

    *

--- src/cz/bliksoft/javautils/net/http/OKHTTPHandler.java
+++ src/cz/bliksoft/javautils/net/http/OKHTTPHandler.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 
 public class OKHTTPHandler extends BasicHTTPHandler {
 
+	public OKHTTPHandler() {
+		addSupportedGETPOST();
+	}
+	
 	@Override
 	public void handle(BSHttpContext context) throws IOException {
 		sendOK(context.httpExchange);

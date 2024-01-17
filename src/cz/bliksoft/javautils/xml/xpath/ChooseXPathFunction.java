74e7efa9bb85dadbfcc1e2db6b312cef17cf4f65
Jakub Jelínek <jakub.jelinek@gmail.com>
2024-01-17 21:27:32 +0100

    backport změn z BS

--- src/cz/bliksoft/javautils/xml/xpath/ChooseXPathFunction.java
+++ src/cz/bliksoft/javautils/xml/xpath/ChooseXPathFunction.java
@@ -9,8 +9,16 @@ public class ChooseXPathFunction implements XPathFunction {
 	@SuppressWarnings("rawtypes")
 	@Override
 	public Object evaluate(List args) throws XPathFunctionException {
+		if (args.size() != 3)
+			throw new XPathFunctionException(
+					"Expecting 3 arguments - boolean, result when tTRUE and result when FALSE. Received " + args.size()
+							+ " arguments.");
 		boolean val;
-		val = (boolean) args.get(0);
+		try {
+			val = (boolean) args.get(0);
+		} catch (Exception e) {
+			throw new XPathFunctionException("Failed to cast first argument to boolean.");
+		}
 		if (val)
 			return args.get(1);
 		else

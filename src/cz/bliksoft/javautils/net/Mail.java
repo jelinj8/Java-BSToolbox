fd282c7c0d8a164dba54269a472588c5d2bf6f85
Jakub Jelínek <jakub.jelinek@veropartners.com>
2024-03-20 10:19:15 +0100

    javax.mail -> jakarta.mail

--- src/cz/bliksoft/javautils/net/Mail.java
+++ src/cz/bliksoft/javautils/net/Mail.java
@@ -3,12 +3,12 @@ package cz.bliksoft.javautils.net;
 import java.util.Properties;
 import java.util.logging.Logger;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 
 /**
  * To uise please add

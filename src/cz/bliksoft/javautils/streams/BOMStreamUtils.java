9d0fb12b872840b34b3d4da65f51db804eeeeb51
Jakub Jelínek <jakub.jelinek@veropartners.com>
2024-03-20 09:37:27 +0100

    *

--- src/cz/bliksoft/javautils/streams/BOMStreamUtils.java
+++ src/cz/bliksoft/javautils/streams/BOMStreamUtils.java
@@ -8,38 +8,106 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 import org.apache.commons.io.ByteOrderMark;
 import org.apache.commons.io.input.BOMInputStream;
+import org.apache.commons.io.input.CloseShieldInputStream;
 
 /**
  * helper to ignore UTF8 BOM in files/streams
  */
 public class BOMStreamUtils {
 
-	public static Reader getReader(File source) throws IOException {
+	/**
+	 * wrapper for {@link #getReader(InputStream, Charset) getReader(InputStream,
+	 * Charset) with default or detected charset}
+	 * 
+	 * @param source
+	 * @return
+	 * @throws IOException
+	 */
+	public static BufferedReader getReader(File source) throws IOException {
 		InputStream is = new FileInputStream(source);
 		return getReader(is);
 	}
 
-	public static Reader getReader(File source, Charset chs) throws IOException {
+	/**
+	 * wrapper for {@link #getReader(InputStream, Charset) getReader(InputStream,
+	 * Charset)}
+	 * 
+	 * @param source
+	 * @param chs
+	 * @return
+	 * @throws IOException
+	 */
+	public static BufferedReader getReader(File source, Charset chs) throws IOException {
 		InputStream is = new FileInputStream(source);
 		return getReader(is, chs);
 	}
 
-	public static Reader getReader(InputStream source) throws IOException {
+	/**
+	 * wrapper for {@link #getReader(InputStream, Charset) getReader(InputStream,
+	 * Charset) with default or detected charset}
+	 * 
+	 * @param source
+	 * @return
+	 * @throws IOException
+	 */
+	public static BufferedReader getReader(InputStream source) throws IOException {
 		return getReader(source, null);
 	}
 
-	public static Reader getReader(InputStream source, Charset chs) throws IOException {
+	/**
+	 * get BufferedReader, skipping UTF8 BOM if present.
+	 * 
+	 * @param source
+	 * @param chs
+	 *            optional specific charset, default used if null and no UTF8 BOM
+	 *            present, UTF8 if null and UTF8 BOM is present
+	 * @return
+	 * @throws IOException
+	 */
+	public static BufferedReader getReader(InputStream source, Charset chs) throws IOException {
 		BOMInputStream bis = BOMInputStream.builder().setInputStream(source).get();
-		Reader rdr = (chs == null ? new InputStreamReader(bis) : new InputStreamReader(bis, chs));
+		Reader rdr = (chs != null ? new InputStreamReader(bis, chs)
+				: (bis.hasBOM(ByteOrderMark.UTF_8) ? new InputStreamReader(bis, StandardCharsets.UTF_8)
+						: new InputStreamReader(bis)));
 		return new BufferedReader(rdr);
 	}
 
+	/**
+	 * wrapps InputStream to skip UTF8 BOM if present
+	 * @param source
+	 * @return
+	 * @throws IOException
+	 */
+	public static InputStream wrap(InputStream source) throws IOException {
+		return BOMInputStream.builder().setInputStream(source).get();
+	}
+	
+	/**
+	 * Checks type of BOM in file
+	 * 
+	 * @param source
+	 * @return
+	 * @throws IOException
+	 */
 	public static ByteOrderMark identifyBOM(File source) throws IOException {
-		try (InputStream is = new FileInputStream(source);
-				BOMInputStream bis = BOMInputStream.builder().setInputStream(is).get();) {
+		try (InputStream is = new FileInputStream(source)) {
+			return identifyBOM(is);
+		}
+	}
+
+	/**
+	 * checks BOM in input stream, not closing the source stream
+	 * 
+	 * @param source
+	 * @return
+	 * @throws IOException
+	 */
+	public static ByteOrderMark identifyBOM(InputStream source) throws IOException {
+		try (BOMInputStream bis = BOMInputStream.builder().setInputStream(CloseShieldInputStream.wrap(source)).get();) {
 			return bis.getBOM();
 		}
 	}

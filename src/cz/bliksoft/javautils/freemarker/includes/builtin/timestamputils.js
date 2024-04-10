87641ff1deae61cc72d7894646093816eb97f03a
Jakub Jelínek <jakub.jelinek@veropartners.com>
2024-04-10 20:33:11 +0200

    timestamp utils tpl fix

--- src/cz/bliksoft/javautils/freemarker/includes/builtin/timestamputils.js
+++ src/cz/bliksoft/javautils/freemarker/includes/builtin/timestamputils.js
@@ -1,8 +1,8 @@
 function formatDurationSec(ms) {
   const seconds = Math.floor(Math.abs(ms / 1000));
   const h = Math.floor(seconds / 3600);
-  const m = Math.floor((seconds % 3600) / 60);
-  const s = Math.round(seconds % 60);
+  const m = Math.floor((seconds - (h * 3600)) / 60);
+  const s = Math.round(seconds - (m * 60 * 3600));
   const t = [h, m > 9 ? m : h ? '0' + m : m || '0', s > 9 ? s : '0' + s]
     .filter(Boolean)
     .join(':');
@@ -12,8 +12,8 @@ function formatDurationSec(ms) {
 function formatDuration(ms) {
   const seconds = Math.abs(ms / 1000);
   const h = Math.floor(seconds / 3600);
-  const m = Math.floor((seconds % 3600) / 60);
-  const s = seconds % 60;
+  const m = Math.floor((seconds - (h * 3600)) / 60);
+  const s = seconds - (m * 60 * 3600);
   const t = [h, m > 9 ? m : h ? '0' + m : m || '0', s >= 10 ? s.toFixed(3) : '0' + s.toFixed(3)]
     .filter(Boolean)
     .join(':');

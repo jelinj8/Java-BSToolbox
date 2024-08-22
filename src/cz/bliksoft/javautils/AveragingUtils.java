5384c7bb11589acc2ef15c90ed36e5344eb13b0c
Jakub Jelínek <jakub.jelinek@gmail.com>
2024-08-22 10:19:21 +0200

    AveragingUtils

---
+++ src/cz/bliksoft/javautils/AveragingUtils.java
@@ -0,0 +1,67 @@
+package cz.bliksoft.javautils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * helper for recording process averages
+ */
+public class AveragingUtils {
+	
+	/**
+	 * contains mapped values with <sum, count>
+	 */
+	private static Map<String, DoubleObject<Double, Long>> averages = new HashMap<>();
+
+	/**
+	 * add new record to keyed sum
+	 * 
+	 * @param key
+	 * @param value
+	 */
+	public void addToAverage(String key, Double value) {
+		synchronized (averages) {
+			DoubleObject<Double, Long> val = averages.get(key);
+			if (val == null)
+				averages.put(key, new DoubleObject<Double, Long>(value, 1l));
+			else {
+				Double sum = val.getO1();
+				sum += value;
+				Long count = val.getO2();
+				count++;
+			}
+		}
+	}
+
+	/**
+	 * get current keyed average
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public Double getAverage(String key) {
+		synchronized (averages) {
+			DoubleObject<Double, Long> val = averages.get(key);
+			if (val == null)
+				return 0d;
+			else
+				return val.getO1() / val.getO2();
+		}
+	}
+
+	/**
+	 * get recorded value count for a key
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public Long getCount(String key) {
+		synchronized (averages) {
+			DoubleObject<Double, Long> val = averages.get(key);
+			if (val == null)
+				return 0l;
+			else
+				return val.getO2();
+		}
+	}
+}

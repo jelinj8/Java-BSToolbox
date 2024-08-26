208a174a7b376254571eefe5922917161e1dd509
Jakub Jelínek <jakub.jelinek@gmail.com>
2024-08-26 22:15:01 +0200

    FileSourceClassLoader fix

--- src/cz/bliksoft/javautils/classloader/FileSourceClassLoader.java
+++ src/cz/bliksoft/javautils/classloader/FileSourceClassLoader.java
@@ -1,24 +1,24 @@
 package cz.bliksoft.javautils.classloader;
 
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
 
 import javax.tools.JavaFileObject.Kind;
 import javax.tools.SimpleJavaFileObject;
 
-import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.FileUtils;
 
-public class FileSourceClassLoader<U> extends AbstractSourceClassLoader<InputStream, U> {
+public class FileSourceClassLoader<U> extends AbstractSourceClassLoader<File, U> {
 
 	@Override
-	public SimpleJavaFileObject getSource(String className, InputStream src) {
+	public SimpleJavaFileObject getSource(String className, File src) {
 		return new SimpleJavaFileObject(URI.create("file:///" + className.replace('.', '/') + ".java"), Kind.SOURCE) {
 			@Override
 			public CharSequence getCharContent(boolean ignoreEncErrors) {
 				try {
-					return IOUtils.toString(src, StandardCharsets.UTF_8);
+					return FileUtils.readFileToString(src, StandardCharsets.UTF_8);
 				} catch (IOException e) {
 					throw new RuntimeException(e);
 				}

d05eda2332776dc5aa342e15247e2f0103107089
Jakub Jelínek <jakub.jelinek@veropartners.com>
2026-03-31 18:43:54 +0200

    ProxyHandler headers fix

--- src/main/java/cz/bliksoft/javautils/net/http/proxy/HttpClientResponseHandlerImpl.java
+++ src/main/java/cz/bliksoft/javautils/net/http/proxy/HttpClientResponseHandlerImpl.java
@@ -4,23 +4,30 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.hc.core5.http.ClassicHttpResponse;
 import org.apache.hc.core5.http.Header;
 import org.apache.hc.core5.http.HttpException;
+import org.apache.hc.core5.http.io.HttpClientResponseHandler;
 
 import com.sun.net.httpserver.HttpExchange;
 
 import cz.bliksoft.javautils.net.http.BasicHTTPHandler;
-import cz.bliksoft.javautils.net.http.HTTPErrorCodes;
-
-import org.apache.hc.core5.http.io.HttpClientResponseHandler;
 
 public class HttpClientResponseHandlerImpl implements HttpClientResponseHandler<HttpExchange> {
 	HttpExchange httpExchange;
+
+	// hop-by-hop headers must not be forwarded to the client
+	private static final Set<String> HOP_BY_HOP_HEADERS = Collections
+			.unmodifiableSet(new HashSet<>(Arrays.asList("connection", "keep-alive", "transfer-encoding", "te",
+					"trailer", "upgrade", "proxy-authorization", "proxy-authenticate")));
 
 	public HttpClientResponseHandlerImpl(HttpExchange httpExchange) {
 		this.httpExchange = httpExchange;
@@ -39,13 +46,14 @@ public class HttpClientResponseHandlerImpl implements HttpClientResponseHandler<
 		Iterator<Header> it = response.headerIterator();
 
 		Long dataLength = 0l;
-		// copy response headers, except length, to be set by response handler
+		// copy response headers, except hop-by-hop and Content-Length (set by sendResponseHeaders)
 		while (it.hasNext()) {
 			Header h = it.next();
+			String nameLower = h.getName().toLowerCase();
 
-			if (BasicHTTPHandler.HEADER_CONTENT_LENGTH.toLowerCase().equals(h.getName().toLowerCase())) {
+			if (BasicHTTPHandler.HEADER_CONTENT_LENGTH.toLowerCase().equals(nameLower)) {
 				dataLength = Long.parseLong(h.getValue());
-			} else {
+			} else if (!HOP_BY_HOP_HEADERS.contains(nameLower)) {
 				List<String> hVal = httpExchange.getResponseHeaders().get(h.getName());
 				if (hVal == null) {
 					hVal = new ArrayList<>(1);
@@ -60,7 +68,7 @@ public class HttpClientResponseHandlerImpl implements HttpClientResponseHandler<
 		// copy body
 		try (OutputStream os = httpExchange.getResponseBody()) {
 			try (InputStream is = response.getEntity().getContent()) {
-				httpExchange.sendResponseHeaders(HTTPErrorCodes.OK.getValue(), dataLength);
+				httpExchange.sendResponseHeaders(response.getCode(), dataLength);
 				IOUtils.copy(is, os);
 			}
 		}
